8ca0fc45386566c5ad5e41f6504a6458bd17a1a1
cryvnn <dreamingisnotharmfvl@gmail.com>
2025-12-29 00:09:47 +0100

    refactor init.lua, rewrite colorscheme in lua and move to lua/colors

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 local utils = require("utils")
 
+require("colors.twilight")
+
 require("config.options")
 local km = require("config.keymaps")
 require("config.lazynvim")
@@ -7,8 +9,6 @@ km.after_lazy_keymaps()
 require("config.autocmds")
 
 utils.color_overrides.setup_colorscheme_overrides()
-
-vim.cmd.colorscheme("twilight")
 
 utils.fix_telescope_parens_win()
 utils.dashboard.setup_dashboard_image_colors()
